add89daa2eeb2b645794be8d2b2a67927569ab8e
Luis Henrique Pereira <lh.p@hotmail.com>
2020-07-28 18:57:02 -0300

    Cadastro de categoria

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Menu from '../Menu';
 import Footer from '../Footer';
+import styled from 'styled-components';
 
-function PageDefault () {
+const Main = styled.main`
+    background-color: var(--black);
+    color: var(--white);
+    flex: 1;
+    padding-top: 50px;
+    padding-right: 5%;
+    padding-left: 5%;
+`;
+
+function PageDefault ({ children }) {
     return (
-        <div>
+        <>
             <Menu />
-
+                <Main>
+                    {children}
+                </Main>
             <Footer />
-        </div>
+        </>
     )
 };
 
