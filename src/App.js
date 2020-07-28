60eaa67d526213e6b3d5cfa2d9183635089f3e3c
Luis Henrique Pereira <lh.p@hotmail.com>
2020-07-28 15:23:51 -0300

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Strawberry Fields Forever - The Beatles, composta pela dupla Lennon-McCartney. Foi inspirada nas lembranças de Lennon a brincar no jardim de Strawberry Field, um orfanato do Exército da Salvação próximo de onde ele morava. Uma das mais belas músicas da maior banda de todos os tempos!"}
       />
 
       <Carousel
